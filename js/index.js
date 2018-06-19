3dc661032463f8152e15c831ce99e4f585022554
vusi <baloyi.trev@gmail.com>
2018-06-19 16:17:46 +0200

    directions

---
+++ js/index.js
@@ -0,0 +1,19 @@
+var resortsFactory = function (resortsArrayData) {
+
+
+
+    var displayResorts = function (arrayToDisplay) {
+        //put the array into an object 
+        //compile the template with the resortsArray and set innerHTML
+
+    }
+
+    var sortByLocation = function (location) {
+        //compare the resort locations against the user's location and sort them from closest to furthest
+        //call the displayResorts function with the array from the above line.
+    }
+
+    var sortByRating = function () {
+        //sort the arrays by user rating from highest to lowest and call the displayResorts function with the sorted array
+    }
+}

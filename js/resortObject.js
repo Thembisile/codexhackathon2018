932549d233959a15ed74727785dc45730901654c
Thembisile <shaundamon6@icloud.com>
2018-06-20 10:38:04 +0200

    initial commit

---
+++ js/resortObject.js
@@ -0,0 +1,154 @@
+let resorts = {
+  name : 'Blue Waters Resort',
+  description : 'It is located a mere 5-minute walk away from Blue Waters Beach and has spots for Braai and Picnic and enjoy the extensive birdwatching of the resort',
+  location : 'Blue Waters Bay',
+  contact:  0214445585,
+  email: 'Ferosa.Maurice@capetown.gov.za',
+  activities: 'Camping and caravanning, Picnics and braaing Fishing',
+  available: 'Seasonal'
+}
+
+let resorts2 = {
+  name : 'Monwabisi',
+  description : 'Monwabisi is a coastal resort that mainly caters for the residents of nearby Khayelitsha. There are paddling pools and a pavilion, with kiosks and ablution facilities.',
+  location : 'Off Baden Powell Drive, Khayelitsha',
+  contact: 0214445584,
+  email: 'thembeka.mdunyelwa@capetown.gov.za',
+  activities: 'Tidal pool, Picnic and braai areas, Ablution amenities, Conference room, Swimming pool, Camp site(s), Chalets.'
+}
+
+let resorts3 = {
+  name : 'Silwerstroomstrand Resort',
+  description : 'Silwerstroomstrand Resort is situated alongside a long stretch of attractive coastline with a small bay to the south (Skulpbaai) and the Silwerstroom River to the north.  The resort offers conference facilities for approximately 20 people and can be booked for use during the week and over weekends.',
+  location : 'Silwerstroom Road, off West Coast Road ',
+  contact:  0214447967,
+  email: '',
+  activities: 'Beach access, Camping and caravanning, Picnics and braaing, Swimming, Fishing',
+  available: 'Seasonal'
+}
+
+let resorts4 = {
+  name : 'Soetwater Resort',
+  description : 'Soetwater is a coastal resort located on a narrow strip of land between the sea and Slangkop Lighthouse. An entrance fee is payable and visitor numbers are controlled to avoid overcrowding. The resort is a popular destination for get-togethers on summer weekends. ',
+  location : 'Lighthouse Road, Kommetjie',
+  contact:  02140446770,
+  email: '',
+  activities: 'Picnics and braaing, Camping and caravanning, Swimming, Fishing',
+  available: 'Seasonal'
+}
+
+let resorts5 = {
+  name : 'Voortrekker Park',
+  description : 'Voortrekker Park is a family friendly resort close to Strand Beach. The resort includes a conference facility that caters for approximately 40 people and can be booked during the week and over weekends.',
+  location : 'Hofmeyer Road, Strand0',
+  contact:  0214446918,
+  email: '',
+  activities: 'Camping and caravanning, Swimming',
+  available: 'Seasonal'
+}
+
+let resorts6 = {
+  name : 'Zandvlei Resort',
+  description : 'Zandvlei Resort is located next to the Zandvlei Estuary Nature Reserve in Muizenberg. The resort offers caravan park visitors a variety of water sports such as sailing, windsurfing and canoeing. ',
+  location : 'Cnr Windermere and Row streets, Muizenberg',
+  contact:  0217026071,
+  email: '',
+  activities: 'Picnics and braaing, Beach access, Camping and caravanning, Swimming, Water sports, Fishing',
+  available: 'Seasonal'
+}
+
+let resorts7 = {
+  name : 'Hendon Park',
+  description : 'Hendon Park is located in Gordon’s Bay along Faure Marine Drive (R44) between Gordon’s Bay Beach and Harbour Island. It is within walking distance to Bikini Beach which is a Blue Flag beach. ',
+  location : 'Faure Marine Drive, Gordon’s Bay',
+  contact:  0218569622,
+  email: '',
+  activities: 'Picnics and braaing, Camping and caravanning, Swimming, Fishing',
+  available: 'Seasonal'
+}
+
+let resorts8 = {
+  name : 'Kogel Bay Resort',
+  description : 'Kogel Bay Resort is located between Gordon’s Bay and Rooi Els, and borders Clarens Drive, a 20 kilometre scenic route that boasts whale-watching opportunities along largely undeveloped parts of the coastline. The resort is situated between high mountains; surrounded by indigenous fynbos and a four kilometre sandy beach; and forms part of the Kogelberg Biosphere Reserve.',
+  location : 'Clarens Drive, Gordon’s Bay',
+  contact: 0218569620 ,
+  email: '',
+  activities: 'Picnics and braaing, Camping and caravanning, Swimming, Fishing',
+  available: 'Seasonal'
+}
+
+let resorts9 = {
+  name : 'Kuilsriver Caravan Resort',
+  description : 'Kuilsriver Caravan Resort is a great place for a family getaway that offers camping, caravan and braai facilities as well as a conference room, swimming pool, and children’s play area.',
+  location : '131 Van Riebeeck Road, Kuilsriver',
+  contact:  0214440268 ,
+  email: 'Anam.Marawu@capetown.gov.za',
+  activities: 'Camping and caravanning, Picnics and braaing, Swimming',
+  available: 'Seasonal'
+}
+
+let resorts10 = {
+  name : 'Macassar Resort',
+  description : 'Macassar Resort offers spectacular views over False Bay and the Gordon’s Bay mountains, due to its elevated location on the dunes. It is adjacent to Macassar Dunes Nature Conservation Area and the Eerste River Estuary.',
+  location : 'Beach Road, Macassar',
+  contact:  0214446919,
+  email: '',
+  activities: 'Camping and caravanning, Picnics and braaing, Swimming',
+  available: 'Seasonal'
+}
+
+let resorts11 = {
+  name : 'Miller’s Point Resort',
+  description : 'Miller’s Point Resort is located five kilometres south of Simon’s Town. Impressively large boulders define the shoreline. There are partly rocky, partly sandy beaches, sheltered swimming areas, secluded nooks, varied walking trails and enclosed bays.',
+  location : 'Main Road (M65), Murdock Valley',
+  contact:  0217866012,
+  email: 'seipati.sefako@capetown.gov.za',
+  activities: 'Camping and caravanning, Picnics and braaing, Swimming & Walking',
+  available: 'Seasonal'
+}
+
+let resorts12 = {
+  name : 'Fish Hoek Caravan Park',
+  description : 'Fish Hoek Caravan Park is conveniently located less than a five-minute walk from Fish Hoek Train Station and a wide variety of shops and restaurants. Set on one of the most beautiful beaches in the Cape, the caravan park amenities include showers, a coin operated laundry and electricity hook ups.',
+  location : 'Beach Road, Fish Hoek',
+  contact:  0217825503,
+  email: '',
+  activities: 'Camping and caravanning, Picnics and braaing, Swimming, Walking & Water Sports',
+  available: 'Seasonal'
+}
+
+let resorts13 = {
+  name : 'Harmony Park',
+  description : 'Harmony Park is situated on the coast between Strand and Gordon’s Bay. The resort offers spectacular views over False Bay, the mountains above Gordon’s Bay and the Hottentots Holland and Table Mountain ranges. A number of shopping centres are located within a three kilometre radius of the resort.',
+  location : 'Cnr Jan Bruin and Gordons Bay roads, Gordons Bay',
+  contact:  0214446918,
+  email: '',
+  activities: 'Camping and caravanning, Picnics and braaing, Swimming, Beach access'
+}
+
+let resorts14 = {
+  name : '',
+  description : '',
+  location : '',
+  contact:  ,
+  email: '',
+  activities: ''
+}
+
+let resorts15 = {
+  name : '',
+  description : '',
+  location : '',
+  contact:  ,
+  email: '',
+  activities: ''
+}
+
+let resorts16 = {
+  name : '',
+  description : '',
+  location : '',
+  contact:  ,
+  email: '',
+  activities: ''
+}
